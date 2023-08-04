935060aa175e4faeb70826f209229ef4d2360852
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-08-04 22:21:17 +0200

    :triangular_flag_on_post: Finished game updater controller

--- src/controllers/update/game.controller.js
+++ src/controllers/update/game.controller.js
@@ -1,5 +1,18 @@
+const path = require('path');
+const getDirs = require('../../utils/getDirs');
+const getFiles = require('../../utils/getFiles');
+
 function getGameData(req, res) {
-    res.json({});
+    const gameFilesPath = path.join(__dirname, '../../../public/game/');
+
+    let folders = getDirs(gameFilesPath);
+    let files = getFiles(gameFilesPath, folders);
+
+    res.json({
+        success: true,
+        folders,
+        files
+    });
 }
 
 module.exports = getGameData;
