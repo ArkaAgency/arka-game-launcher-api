a0cbff97f111f05b17c0f5459e0ffbadce6d3d0c
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-08-04 22:20:20 +0200

    :sparkles: Added getFiles utility

---
+++ src/utils/getFiles.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const _path = require('path');
+const { createHash } = require('crypto');
+
+function getFiles(path, dirs) {
+    let files = [];
+
+    dirs.forEach(dirName => {
+        const dirPath = _path.join(path, dirName);
+        fs.readdirSync(dirPath).forEach((f) => {
+            const filename = _path.join(dirPath, f);
+            const isFile = fs.lstatSync(filename).isFile();
+            if (isFile) {
+                const buff = fs.readFileSync(filename);
+                const hash = createHash('md5').update(buff).digest('hex');
+                const fileObj = {
+                    filename: _path.join(dirName, f).replaceAll('\\', '/'),
+                    downloadLink: 'http://localhost:4004/download/game/' + (encodeURIComponent(_path.join(dirName, f).replaceAll('\\', '/'))),
+                    md5: hash
+                };
+                files.push(fileObj);
+            }
+        });
+    });
+
+    return files;
+}
+
+module.exports = getFiles;
