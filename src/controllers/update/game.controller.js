7a44da2899d5839d823c4a64c154f9c7a4486bd8
codebyalexx <a.legal.contact@gmail.com>
2025-03-23 17:40:17 +0100

    :sparkles: Added get game version route

--- src/controllers/update/game.controller.js
+++ src/controllers/update/game.controller.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const getDirs = require("../../utils/getDirs");
 const getFiles = require("../../utils/getFiles");
+const { existsSync, readFileSync } = require("fs");
 
 async function getGameData(req, res) {
   const gameFilesPath = path.join(__dirname, "../../../public/game/");
@@ -15,4 +16,31 @@ async function getGameData(req, res) {
   });
 }
 
-module.exports = getGameData;
+async function getGameVersion(req, res) {
+  const gameVersionFilePath = path.join(
+    __dirname,
+    "../../../public/game/version.ksf"
+  );
+
+  console.log(gameVersionFilePath);
+
+  if (!existsSync(gameVersionFilePath))
+    return res.json({
+      success: false,
+      error: {
+        message: {
+          en: "The version file cannot be found on remote",
+          fr: "La fichier de version n'a pas pu etre trouve sur le serveur distant",
+        },
+      },
+    });
+
+  const gameVersionFile = readFileSync(gameVersionFilePath);
+
+  return res.json({
+    success: true,
+    version: gameVersionFile.toString(),
+  });
+}
+
+module.exports = { getGameData, getGameVersion };
--- src/routes/update/game.routes.js
+++ src/routes/update/game.routes.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const getGameData = require('../../controllers/update/game.controller');
+const express = require("express");
+const {
+  getGameData,
+  getGameVersion,
+} = require("../../controllers/update/game.controller");
 const router = express.Router();
 
-router.get('/', getGameData);
+router.get("/", getGameData);
+router.get("/version", getGameVersion);
 
 module.exports = router;
